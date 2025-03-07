e6fdf756d3265f163a317b48690b25453f7070c5
leon12-hub <vd8251@gmail.com>
2025-03-07 11:06:22 +0530

    Add files via upload

---
+++ reasons.js
@@ -0,0 +1,23 @@
+// Reasons database
+const reasons = [
+    {
+        text: "we should enjoy evey last moment of college 😊",
+        emoji: "🌟",
+        gif: ""
+    },
+    {
+        text: "I never should go to pondicherry again ",
+        emoji: "😆",
+        gif: "gif2.gif"
+    },
+    {
+        text: "we are friends forever with the small fighting and get together after ending of the fight.",
+        emoji: "👂",
+        gif: "gif1.gif"
+    },
+    {
+        text: "Our inside jokes are literally the best thing ever~",
+        emoji: "🚀",
+        gif: "gif2.gif"
+    }
+];
